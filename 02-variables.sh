f3456065fe0edaa01296ddfb74cbcaef461f22e4
Widline Hyacinthe <3viii2316@gmail.com>
2018-12-03 10:02:32 -0500

    02 Finalisé

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+prenom=KimCharle
+echo $prenom
+# Forcer la déclaration d'une variable "int"
+declare -i age=33
+# Mettre une variable READ ONLY
+declare -r age2=33
+
+# Les variables système
+echo $0
+echo $1
+echo $2
+
+echo $#
