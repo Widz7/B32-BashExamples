12040c3d6582ec20c4b5c0ac4c51bd0033caf708
Widline Hyacinthe <3viii2316@gmail.com>
2018-12-03 12:28:05 -0500

    exercise 1

--- exercice1.sh
+++ exercice1.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-
+read -p "entrez un mot: " mot
 # La ligne suivante permet de vérifier si un mot existe dans le dictionnaire
 # Il faut suffixer cet URL avec le mot entré par l'uasger (ex: word=chat)
-#wget -qO - http://dictionary.objectif8.com/exists.php?word=
+liste=`wget -qO - http://dictionary.objectif8.com/exists.php?word=$mot`
+if test  $liste  = 1
+then
+        echo "Ce mot existe"
+else 
+        echo "Ce mot n'existe pas"
+fi
+
+
--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+menu(){
+
+echo         "========CVM=========="
+echo         "                     "
+echo         "   d= dictionnaire   "
+echo         "   q= quitter        "
+echo         "                     " 
+echo         "====================="
+}
+menu #apelle de la fonction pour  affichage lors execution
+choix=z
+while [[ $choix != "q" ]]
+do
+  menu
+  read -p "Choix : " choix
+  if  [[ $choix =  "d" ]] 
+   then
+              ./exercice1.sh
+               sleep 1
+  fi
+done
+
+clear
