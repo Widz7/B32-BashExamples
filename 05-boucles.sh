61566a18ba6fcb8f45f866e6c26bc746bf29a7c6
Widline Hyacinthe <3viii2316@gmail.com>
2018-12-03 10:51:38 -0500

    Boucle terminé

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
 
+for ((i=0;i<5;i++))
+do
+
+          echo $i
+done
+
+liste = `ls`
+
+for fic in $liste
+do 
+
+           echo $fic
+done
+
+resultat=o
+#-o = or
+#-a = and
+
+while test $resultat = o
+do
+            read -p "Entrez o pour répéter la boucle : " resultat
+done
