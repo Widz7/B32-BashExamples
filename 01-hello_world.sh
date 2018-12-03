80ce2ca6e83949986848f9d0788ab5c1c2f290b6
Widline Hyacinthe <3viii2316@gmail.com>
2018-12-03 09:50:07 -0500

    IM done with you hello world1

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+echo "Salut"
+# this is a comment
+
+echo -n "vive "
+echo "LAPiZZA"
+# -n =  empeche retour de ligne
+echo -e "123\n123\n123"
+# -e = pour les caractere speciaux oomme les back slash
+read -p "Vous avez quel age?" age 
+echo $age "ans! vous paraissez plus jeune"
