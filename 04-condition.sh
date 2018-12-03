dc511e9107e05193ac200546f1a03b58263b10af
Widline Hyacinthe <3viii2316@gmail.com>
2018-12-03 10:40:34 -0500

    numero 4 Complet

--- 04-condition.sh
+++ 04-condition.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
+note=60
 
+if test $note -lt 60
+then
+          echo "DSL vous devez reprendre le cours"
+elif [[ $note -eq 60 ]]
+then
+echo "Vous êtes à la limite!!"
+else
+echo "Bravo!"
+fi
 
+read lettre
 
+case $lettre in
+            [[:lower:]])
+             echo "minuscule"
+               ;;
+            *)
+             echo "autre..."
+esac
+
+if test $lettre != "a"
+then
+             echo "La lettre n'est pas a"
+fi
 
 
 
