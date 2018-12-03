5474d7e7075a9ec5e86bdd709d48d54eab981782
Widline Hyacinthe <3viii2316@gmail.com>
2018-12-03 11:17:47 -0500

    Existance d'un fichier document

--- 09-fichiers.sh
+++ 09-fichiers.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+if test -e $1
+then
+              echo "existe"
+               if test -d $1
+              then
+                      echo "C'est un répertoire"
+              fi
+else
+              echo "inexistant"
+fi
+
+
+
+# -e voir si il existe
+# -d voir si c'est une répertoire
